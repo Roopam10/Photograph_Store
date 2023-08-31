d6251089896a92077cde4d21b4471a42373985ee
Roopam10 <roopampatil10@gmail.com>
2023-08-31 15:57:20 +0530

    footer changed

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Footer =()=>{
     return (
         <div className='footer'>
-            <h3>Footer for Photographs</h3>
+            <h3>Roopam Patil - IIIT Bangalore</h3>
+            <a href='https://github.com/Roopam10/Photograph_Store' target='_blank'><h3>My github</h3></a>
         </div>
     )
 }
